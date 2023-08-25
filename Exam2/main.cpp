3af5fd9303f61529df4b5ba34131b3b610f0ed96
ntran210900 <111794576+ntran210900@users.noreply.github.com>
2023-08-25 14:43:50 -0700

    Update main.cpp

--- Exam2/main.cpp
+++ Exam2/main.cpp
@@ -13,6 +13,7 @@ void OptionB(PeriodicTable *table);
 //description: this function will display the periodic table and the option for the program.
 char menuOption() {asdawdasd
 asdasdawdasdasdsadwadsad
+		   testasd22
     cout << "\n\t" << string(100, char(205)) << "\n";
     cout << "\n\t" << string(100, char(205));
     cout << "\n\tA. Dynamic Array of Structure Elements";
