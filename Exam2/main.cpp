a2ce491cb75827e47c665ad354fdb78ce8b92845
ntran210900 <111794576+ntran210900@users.noreply.github.com>
2023-08-25 14:45:04 -0700

    atestUpdate main.cpp

--- Exam2/main.cpp
+++ Exam2/main.cpp
@@ -12,7 +12,12 @@ void OptionB(PeriodicTable *table);
 //postcondition: none.
 //description: this function will display the periodic table and the option for the program.
 char menuOption() {
+<<<<<<< HEAD
 	sssssssss
+=======
+    asdwasdwd
+        ssaasdasws
+>>>>>>> atestUpdate main.cpp
     cout << "\n\t" << string(100, char(205)) << "\n";
     cout << "\n\t" << string(100, char(205));
     cout << "\n\tA. Dynamic Array of Structure Elements";
