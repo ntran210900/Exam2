53bac95c58139f1e5242d66e99bc29984590db7b
Nguyen Tran <ntran210900@gmail.com>
2023-08-25 14:52:58 -0700

    #Commit: create a new brand | Testtest

--- Exam2/main.cpp
+++ Exam2/main.cpp
@@ -12,10 +12,6 @@ void OptionB(PeriodicTable *table);
 //postcondition: none.
 //description: this function will display the periodic table and the option for the program.
 char menuOption() {
-    asdwasdwd
-        ssaasdasws
-asdawe21
-		   asdawdawdawdasdas
     cout << "\n\t" << string(100, char(205)) << "\n";
     cout << "\n\t" << string(100, char(205));
     cout << "\n\tA. Dynamic Array of Structure Elements";
