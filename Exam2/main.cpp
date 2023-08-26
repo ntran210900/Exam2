d49c7e6f524f6bfa447aae2b5f4aba8eb4e9609e
warokun <justdrew0624@gmail.com>
2023-08-25 18:23:03 -0700

    Original

--- Exam2/main.cpp
+++ Exam2/main.cpp
@@ -11,13 +11,8 @@ void OptionB(PeriodicTable *table);
 //precodition: none.
 //postcondition: none.
 //description: this function will display the periodic table and the option for the program.
-char menuOption() {
-a
-<<<<<<<<< Temporary merge branch 1
-=========
-
-hello world let me helpy ou
->>>>>>>>> Temporary merge branch 2
+char menuOption() 
+{
     cout << "\n\t" << string(100, char(205)) << "\n";
     cout << "\n\t" << string(100, char(205));
     cout << "\n\tA. Dynamic Array of Structure Elements";
